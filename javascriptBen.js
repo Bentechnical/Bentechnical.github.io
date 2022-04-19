198541dbb75e542845b8cb24611ea163548540f7
Bentechnical <ben.mark.taylor@gmail.com>
2022-04-18 23:12:08 -0400

    comit

--- javascriptBen.js
+++ javascriptBen.js
@@ -7,10 +7,20 @@ var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("type-writer").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
-    
-    
   }
 }
+$(document).ready(function () {
+    $('i').css('display', 'none');
+    setTimeout(function(){
+        function blink_text() {
+            $('i').fadeIn(1500);
+            $('i').fadeOut(1200);
+        }
+        setInterval(blink_text, 3000);
+    }, 4500); 
+
+});
+
